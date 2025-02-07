2ca1d9cd91b4b1ea6a3f3af685614eaa318f73b9
IslamElsaqqa <islamashrafmahmoud5@gmail.com>
2025-02-07 16:21:38 +0200

    commiting database

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,22 +7,15 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 } 
     ])
 
-    const [name,setName] = useState('Islam')
     const handleDelete = (id) => { 
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    useEffect(() => {
-        console.log('Hello, Run!'); // Runs Every render!
-    },[name]);
-
     return (  
         <div className="Home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's blogs" /> */}
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{ name}</p>
+            
         </div>
     )
 }
