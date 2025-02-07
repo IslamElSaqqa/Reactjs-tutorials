ec06327616b94d891a8a9f490f047b213efd6845
IslamElsaqqa <islamashrafmahmoud5@gmail.com>
2025-02-07 18:53:17 +0200

    Committing Fetching data with USE HOOK

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 } 
-    ])
+    const [blogs, setBlogs] = useState(null);
+    const [IsPending, setIsPending] = useState(true);
 
-    const handleDelete = (id) => { 
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+
+    useEffect(() => {
+        fetch('http://localhost:8002/blogs')
+            .then(res => res.json()) // Return the parsed JSON
+            .then(data =>{
+                setBlogs(data)
+                setIsPending(false)
+            })
+            .catch(error => console.error("Error fetching blogs:", error));
+    }, []); // Add useEffect to prevent re-fetching on every render
+        // initial dependency array to prevent re-fetching every render
 
     return (  
         <div className="Home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {IsPending && <div><h2>Loading ....</h2></div>} 
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             
         </div>
     )
